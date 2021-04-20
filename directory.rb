c0b760893add49ad7dbbb31ec9f2a89f2ab659fe
Adam <adam.downey@me.com>
2021-04-20 14:29:07 +0100

    students no longer hardcoded

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-# list of students first
-students = [ 
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Racthed", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Kreuger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffery Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+
+# create and fill student array
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+# create array  for students instead of hardcode
+  students = []
+# request name
+  name = gets.chomp
+# so while name is not empty do this
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+# ask for name again
+    name  = gets.chomp
+  end
+  # returns students array
+  students
+end
 # print all students
 def print_header
   puts "The students of Villains Academy"
@@ -27,6 +32,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 # actually call for something to happen
+students = input_students
 print_header
 print(students)
 print_footer(students)
