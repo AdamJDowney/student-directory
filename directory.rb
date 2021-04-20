ace958f577a3cfaab66aa0f675498cc922120082
Adam <adam.downey@me.com>
2021-04-20 13:41:42 +0100

    data now in  hash

--- directory.rb
+++ directory.rb
@@ -1,30 +1,30 @@
 # list of students first
 students = [ 
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Racthed",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Kreuger",
-  "The Joker",
-  "Joffery Baratheon", 
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Racthed", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Kreuger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffery Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 # print all students
 def print_header
   puts "The students of Villains Academy"
   puts "------------"
 end
-def print(names)
-  names.each do |name|  
-  puts name
+def print(students)
+  students.each do |student|  
+  puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 # print total
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 # actually call for something to happen
 print_header
