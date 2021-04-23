779619ee5abc05760e87b5ab35263d9487768e90
Adam <adam.downey@me.com>
2021-04-23 12:39:08 +0100

    loading from cmd line

--- directory.rb
+++ directory.rb
@@ -31,26 +31,6 @@ def print_menu
   puts "9. Exit"
 end
 
-def save_students
-  file = File.open("students.csv", "w")
-  # iterate over array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
-end
-
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
 def show_students
   print_header
   print_students_list
@@ -61,11 +41,11 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   students = []
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name  = gets.chomp
+    name  = STDIN.gets.chomp
   end
 end
 
@@ -83,4 +63,37 @@ end
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
+
+def save_students
+  file = File.open("students.csv", "w")
+  # iterate over array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from cmd line
+  return if filename.nil? # leave method if not given
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{students.count} from #{filename}"
+  else # doest exist
+    puts "sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+try_load_students
 interactive_menu
